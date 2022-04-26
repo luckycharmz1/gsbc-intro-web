221667279851fc71466a2bf132eb9562c062105e
Charmaine Washington <98441068+luckycharmz1@users.noreply.github.com>
2022-04-26 15:33:28 -0700

    Update algorithm.js

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -3,3 +3,37 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
+
+const numbers = [];
+
+for (let i = 0; i <= 100; i++) {
+  numbers[i] = Math.floor(Math.random() * 1000);
+}
+//Formats the numbers
+let nLen = numbers.length;
+let text = "";
+
+for (let i = 0; i < nLen; i++) {
+  text += numbers[i] + ", ";
+
+    if (i == 100) {
+      text += numbers[i] + ", and ";
+      }
+    if (i == 100) {
+      text += numbers[i] + ".";
+      }
+  }
+document.getElementById("numbers").innerHTML = text;
+
+//Sorts the numbers and pops the largest one
+numbers.sort(function (a, b) {return a - b;});
+document.getElementById("largest_number").innerHTML = numbers.pop();
+
+//Adds all the numbers together
+let s = 0;
+let nmLen = numbers.length;
+
+for (let i = 0; i < nmLen; i++) {
+  s = s + numbers[i];
+  }
+document.getElementById("sum").innerHTML = s;
