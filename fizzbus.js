4e629ec9a501ba00a1b5bd27d466139ef2300a12
Charmaine Washington <98441068+luckycharmz1@users.noreply.github.com>
2022-04-26 17:12:07 -0700

    Create fizzbus.js

---
+++ fizzbus.js
@@ -0,0 +1,37 @@
+
+/**
+ * @param{number} n
+ * @returns{string[]}
+ */
+ function fizzbuzz(n) {
+    var values = []
+    for (let i = 1; i <= n; i++) {
+        var isMultipleOfThree = (i % 3 == 0)
+        var isMultipleOfFive = (i % 5 == 0)
+
+        if (isMultipleOfFive && isMultipleOfThree) {
+            values.push("FizzBuzz")
+        }
+        else if (isMultipleOfThree) {
+            values.push("Fizz")
+        }
+        else if (isMultipleOfFive) {
+            values.push("Buzz")
+        }
+        else {
+            values.push(i.toString())
+        }
+    }
+    return values
+}
+
+// Calculate the results of Fizz Buzz for all the numbers to 50.
+var results = fizzbuzz(50)
+
+// Print the results by appending paragraph nodes to the document body.
+for (var result of results)
+{
+    let textNode = document.createElement("p")
+    textNode.textContent = result
+    document.body.appendChild(textNode)
+}
